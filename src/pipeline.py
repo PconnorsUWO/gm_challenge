96c843d5ab96baec2b1579fa9b6ca5b9773f4c8f
Thierry Huot <tdhuot@gmail.com>
2025-03-09 10:21:13 -0400

    models

--- src/pipeline.py
+++ src/pipeline.py
@@ -8,7 +8,7 @@ from ultralytics import YOLO
 import helpers  # Ensure this module is available in your PYTHONPATH
 
 # Load your model
-model = YOLO("yolo11x.pt")
+model = YOLO("src\models\yolo11n20epochs.pt")
 
 def process_directory(input_dir, results_path):
     # Load images along with their file paths
